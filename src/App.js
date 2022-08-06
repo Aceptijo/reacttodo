dff10f5142d7564d7f497a76bdb4312805c02857
Igor <grinev98@mail.ru>
2022-08-06 19:55:57 +0300

    132

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.sass';
 import Content from './components/Content/Content';
-// import ContentWrapper from './components/ContentWrapper/ContentWrapper';
 import Navbar from './components/Navbar/Navbar';
-// import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
    return (
       <div className='App'>
          <Navbar />
-         {/* <Sidebar />
-         <ContentWrapper /> */}
          <Content />
       </div>
    );
